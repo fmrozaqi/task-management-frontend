26fcbc4bf5182595c7dd4892c4cf1936c177e324
fmrozaqi <fmrozaqi@users.noreply.github.com>
2021-08-04 04:10:17 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.c8b68002747dd7e50136fea0fb3c3a07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64460b6395b718c5ca457b5a54a1c6a",
+    "url": "https://fmrozaqi.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "19fdacd520d48dd216df",
+    "url": "https://fmrozaqi.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "1da78085e106748ab10d",
+    "url": "https://fmrozaqi.github.io/task-management-frontend/static/js/2.09d98438.chunk.js"
+  },
+  {
+    "revision": "19fdacd520d48dd216df",
+    "url": "https://fmrozaqi.github.io/task-management-frontend/static/js/main.57bb6598.chunk.js"
+  },
+  {
+    "revision": "73be9a3165e126dd608f",
+    "url": "https://fmrozaqi.github.io/task-management-frontend/static/js/runtime~main.d6e73d03.js"
+  }
+]);
